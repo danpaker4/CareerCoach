eac89c2ab725a770290e58bf17816df6cb35333a
Gal Kosover <galkosover0503@gmail.com>
2026-01-31 00:05:59 +0200

    progress

--- backend/src/routes/users/user.model.ts
+++ backend/src/routes/users/user.model.ts
@@ -1,14 +1,22 @@
 import { z } from "zod";
 
+export const AchievementSchema = z.object({
+  id: z.uuid(),
+  name: z.string(),
+  grade: z.number()
+});
+
 export const UserSchema = z.object({
-    id: z.string().uuid(),
+    id: z.uuid(),
     firstName: z.string(),
     lastName: z.string(),
-    email: z.string().email(),
+    email: z.email(),
     password: z.string(),
     birthDate: z.date(),
     currentJob: z.string().optional(),
+    linkedInUrl: z.string().optional(),
+    cv: z.string().optional(),
+    achievements: z.array(AchievementSchema)
 });
 
 export type User = z.infer<typeof UserSchema>;
-
